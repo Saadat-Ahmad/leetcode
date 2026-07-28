5b61fc3502322586d8f7470551ef36e89dd60ba4
Syed Saadat Ahmad <66525881+Saadat-Ahmad@users.noreply.github.com>
2026-07-28 08:54:50 +0530

    Time: 40 ms (44.68%) | Memory: 29.5 MB (48.23%) - LeetSync

---
+++ 3451-string-compression-iii/string-compression-iii.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string compressedString(string word) {
+        int i = 0;
+        char c = word[0];
+        string res = "";
+        for(const auto& it : word){
+            if(it == c && i < 9) i++;
+            else{
+                res += to_string(i);
+                res += c;
+                c = it;
+                i = 1;
+            }
+        }
+        res += to_string(i);
+        res += c;
+        return res;
+    }
+};
