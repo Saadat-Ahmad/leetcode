88077fa63385c2ef8da4f356de9e8fb31a94d373
Syed Saadat Ahmad <66525881+Saadat-Ahmad@users.noreply.github.com>
2026-07-28 09:29:08 +0530

    Time: 41 ms (55.89%) | Memory: 59.2 MB (92.98%) - LeetSync

---
+++ 817-design-hashmap/design-hashmap.cpp
@@ -0,0 +1,49 @@
+class LNode {
+public:
+    int val;
+    int key;
+    LNode* next;
+    LNode(int adr, int value){
+        key = adr;
+        val = value;
+        next = nullptr;
+    }
+};
+class MyHashMap {
+public:
+    vector<LNode*> arr {1000, nullptr};
+    MyHashMap() {
+        
+    }
+    
+    void put(int key, int value) {
+        LNode* n = new LNode(key,value);
+        n->next = arr[key%1000];
+        arr[key%1000] = n;
+    }
+    
+    int get(int key) {
+        LNode* n = arr[key%1000];
+        while(n != nullptr){
+            if(n->key == key) return n->val;
+            n = n->next;
+        }
+        return -1;
+    }
+    
+    void remove(int key) {
+        LNode* n = arr[key%1000];
+        while(n != nullptr){
+            if(n->key == key) n->key = -99;
+            n = n->next;
+        }
+    }
+};
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap* obj = new MyHashMap();
+ * obj->put(key,value);
+ * int param_2 = obj->get(key);
+ * obj->remove(key);
+ */
