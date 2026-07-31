29a42518aec3fd85256d3b47a5e620c85200413d
Syed Saadat Ahmad <66525881+Saadat-Ahmad@users.noreply.github.com>
2026-07-31 11:29:47 +0530

    Time: 1 ms (26.05%) | Memory: 10.1 MB (21.20%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        int i = 0, j =0;
+        reverse(a.begin(), a.end());
+        reverse(b.begin(), b.end());
+        string s = "";
+        int carry = 0;
+        int na, nb;
+        int res;
+        while(i < a.size() || j < b.size()){
+            na = (i < a.size())? a[i]-'0' : 0;
+            nb = (j < b.size())? b[i]-'0' : 0;
+            res = na + nb + carry;
+            carry = (res > 1)? 1 : 0;
+            res = res%2;
+            s += to_string(res);
+            i++;
+            j++;
+        }
+        if(carry == 1) s+="1";
+        reverse(s.begin(), s.end());
+        return s;
+    }
+};
