45293e4a32757593f2efcac9a5454c58403823ea
Syed Saadat Ahmad <66525881+Saadat-Ahmad@users.noreply.github.com>
2026-07-31 06:22:30 +0530

    Time: 0 ms (100.00%) | Memory: 10.6 MB (57.63%) - LeetSync

--- 338-counting-bits/counting-bits.cpp
+++ 338-counting-bits/counting-bits.cpp
@@ -1,25 +1,12 @@
 class Solution {
-private:
-    int haming(int& n){
-        int res = 0;
-        for(int i = 0; i < 32; i++){
-            if(n < 0) res++;
-            n = n << 1;
-        }
-        return res;
-    }
 public:
     vector<int> countBits(int n) {
+        int pow = 1;
         vector<int> arr (n+1);
-        int res, tmp;
         for(int i = 0; i <= n; i++){
-            tmp = i;
-            res = 0;
-            while(tmp != 0){
-                if(tmp%2 == 1) res++;
-                tmp = tmp >> 1;
-            }
-            arr[i] = res;
+            if(i == 2*pow) pow *= 2;
+            if(i == 0) arr[i] = 0;
+            else arr[i] = 1 + arr[i-pow]; 
         }
         return arr;
     }
