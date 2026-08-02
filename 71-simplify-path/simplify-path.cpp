634cc48ac25e9f3dd4c02e30b0f15ed13ef82068
Syed Saadat Ahmad <66525881+Saadat-Ahmad@users.noreply.github.com>
2026-08-02 07:23:20 +0530

    Time: 7 ms (17.52%) | Memory: 15.5 MB (5.85%) - LeetSync

--- 71-simplify-path/simplify-path.cpp
+++ 71-simplify-path/simplify-path.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     string simplifyPath(string& path) {
-        stack<string> stk;
+        vector<string> stk;
         string tmp;
         string ans = "";
         path += "/";
@@ -9,18 +9,18 @@ public:
             if(it == '/') {
                 if(tmp == ".") ;
                 else if(tmp == ".."){
-                    if(!stk.empty()) stk.pop();
+                    if(!stk.empty()) stk.pop_back();
                 }
-                else if (tmp.size() > 0) stk.push(tmp);
+                else if (tmp.size() > 0) stk.push_back(tmp);
                 tmp = "";
             } else {
                 tmp += it;
             }
         }
         while(!stk.empty()){
-            tmp = stk.top();
+            tmp = stk.back();
             ans = "/" + tmp + ans;
-            stk.pop();
+            stk.pop_back();
         }
         if (ans.size() == 0) ans = "/";
         return ans;
