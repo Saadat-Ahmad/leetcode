f36d3e2f658fb9455d9430b091f2e9a5c003d771
Syed Saadat Ahmad <66525881+Saadat-Ahmad@users.noreply.github.com>
2026-08-10 15:36:36 +0530

    Time: 6 ms (6.40%) | Memory: 9.4 MB (21.20%) - LeetSync

--- 7-reverse-integer/reverse-integer.cpp
+++ 7-reverse-integer/reverse-integer.cpp
@@ -1,17 +1,14 @@
 class Solution {
 public:
     int reverse(int x) {
-        const int MIN = -2147483648;
-        const int MAX = 2147483647;
-        int res = 0;
-        int digit;
-        while(x != 0){
-            digit = x%10;
-            x /= 10;
-            if(res < MIN/10 || (res == MIN/10 && digit < MIN%10)) return 0;
-            if(res > MAX/10 || (res == MAX/10 && digit > MAX%10)) return 0;
-            res = (res * 10) + digit;
-        }
-        return res;
+        bool ngtv = false;
+        if(x < 0) ngtv = true;
+        // x = abs(x);
+        string s = to_string(x);
+        ::reverse(s.begin(), s.end());
+        int64_t ans = stoll(s);
+        ans = (ngtv)? -1*ans : ans;
+        if(ans < INT_MIN || ans > INT_MAX) return 0;
+        else return (int)ans;
     }
 };
