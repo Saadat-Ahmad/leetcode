2d22ebeeab387762e1b70e7f8680564f7a722c4a
Syed Saadat Ahmad <66525881+Saadat-Ahmad@users.noreply.github.com>
2026-08-10 14:52:45 +0530

    Time: 1173 ms (1.31%) | Memory: 8.7 MB (44.62%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -1,4 +1,18 @@
 class Solution {
+private:
+    double powerFun(double x, int64_t n){
+        double i = 1;
+        double res = x;
+        while(2*i <= n){
+            res *= res;
+            i *= 2;
+        }
+        while(i < n){
+            res *= x;
+            i++;
+        }
+        return res;
+    }
 public:
     double myPow(double x, int64_t n) {
         double res = 1;
@@ -6,18 +20,9 @@ public:
         if(x == -1) return (n%2 == 0)? 1 : -1;
         if(n == INT_MIN) return 0;
         if(x == 0) return 0;
-        if(n < 0){
-            n = abs(n);
-            while(n > 0){
-                res *= x;
-                n--;
-            }
-            return 1/res;
-        }
-        while(n > 0){
-            res *= x;
-            n--;
-        }
+        if(n == 0) return 1;
+        res = powerFun(x, abs(n));
+        if (n < 0) return 1/res;
         return res;
     }
 };
