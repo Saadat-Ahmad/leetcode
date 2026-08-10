b95dc0450ef445e1a7d7aac9eaf421a5858f0fda
Syed Saadat Ahmad <66525881+Saadat-Ahmad@users.noreply.github.com>
2026-08-10 15:11:03 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (44.62%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -13,9 +13,6 @@ private:
 public:
     double myPow(double x, int64_t n) {
         double res = 1;
-        if(x == 1) return 1;
-        if(x == -1) return (n%2 == 0)? 1 : -1;
-        if(n == INT_MIN) return 0;
         if(x == 0) return 0;
         if(n == 0) return 1;
         res = powerFun(x, abs(n));
