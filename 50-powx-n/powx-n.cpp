e870808a964e44203ae496e9c7c3db3e6ffa102b
Syed Saadat Ahmad <66525881+Saadat-Ahmad@users.noreply.github.com>
2026-08-10 14:34:29 +0530

    Time: 2023 ms (1.31%) | Memory: 8.6 MB (71.66%) - LeetSync

---
+++ 50-powx-n/powx-n.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    double myPow(double x, int64_t n) {
+        double res = 1;
+        if(x == 1) return 1;
+        if(x == -1) return (n%2 == 0)? 1 : -1;
+        if(n == INT_MIN) return 0;
+        if(n < 0){
+            n = abs(n);
+            while(n > 0){
+                res *= x;
+                n--;
+            }
+            return 1/res;
+        }    
+        while(n > 0){
+            res *= x;
+            n--;
+        }
+        return res;
+    }
+};
