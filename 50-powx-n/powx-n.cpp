6c8f85383ec531a6350ad4dcbfa32b750238ab2f
Syed Saadat Ahmad <66525881+Saadat-Ahmad@users.noreply.github.com>
2026-08-10 14:36:47 +0530

    Time: 2034 ms (1.31%) | Memory: 8.7 MB (44.62%) - LeetSync

--- 50-powx-n/powx-n.cpp
+++ 50-powx-n/powx-n.cpp
@@ -5,6 +5,7 @@ public:
         if(x == 1) return 1;
         if(x == -1) return (n%2 == 0)? 1 : -1;
         if(n == INT_MIN) return 0;
+        if(x == 0) return 0;
         if(n < 0){
             n = abs(n);
             while(n > 0){
@@ -12,7 +13,7 @@ public:
                 n--;
             }
             return 1/res;
-        }    
+        }
         while(n > 0){
             res *= x;
             n--;
