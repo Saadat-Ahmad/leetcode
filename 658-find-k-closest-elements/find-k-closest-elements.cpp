27a627816c6833e36baf6394ff6c8ad8dd4f8990
Syed Saadat Ahmad <66525881+Saadat-Ahmad@users.noreply.github.com>
2026-08-21 18:33:46 +0530

    Time: 15 ms (39.11%) | Memory: 42 MB (9.47%) - LeetSync

---
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        pair<int, int> pr; //diff, val
+        vector<pair<int, int>> ar;
+        vector<int> res;
+        for(const auto it : arr){
+            pr.first = abs(it - x);
+            pr.second = it;
+            ar.push_back(pr);
+        }
+        sort(ar.begin(), ar.end());
+        for(int i = 0; i < k; i++){
+            res.push_back(ar[i].second);
+        }
+        sort(res.begin(), res.end());
+        return res;
+    }
+};
