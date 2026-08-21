b6a61a158d93a5c09a509f72a41f4e43cc004e44
Syed Saadat Ahmad <66525881+Saadat-Ahmad@users.noreply.github.com>
2026-08-21 18:36:49 +0530

    Time: 24 ms (27.81%) | Memory: 42.1 MB (6.44%) - LeetSync

--- 658-find-k-closest-elements/find-k-closest-elements.cpp
+++ 658-find-k-closest-elements/find-k-closest-elements.cpp
@@ -4,7 +4,7 @@ public:
         pair<int, int> pr; //diff, val
         vector<pair<int, int>> ar;
         vector<int> res;
-        for(const auto it : arr){
+        for(const auto& it : arr){
             pr.first = abs(it - x);
             pr.second = it;
             ar.push_back(pr);
